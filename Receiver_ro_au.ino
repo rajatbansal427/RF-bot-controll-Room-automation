91e1ef0e6cee29d495368ba63f3e972b419b237c
rajatbansal427 <rajatbansal427@gmail.com>
2016-02-20 02:17:49 +0530

    Added files via upload

---
+++ Receiver_ro_au.ino
@@ -0,0 +1,71 @@
+
+#include <VirtualWire.h>
+
+const int led_pin = 13;
+const int receive_pin = 11;
+const int vcc = 10;
+
+#define FALSE 0
+#define TRUE 1
+
+boolean output[4] = {false,false,false,false};
+float output_pin[] = {7,6,5,4};
+void setup()
+{
+ 
+   delay(1000);
+   Serial.begin(9600); 
+   Serial.println("setup");
+   
+   vw_set_rx_pin(receive_pin);
+   vw_setup(2000); // Transmission rate
+   // Start the receiver PLL
+   vw_rx_start();
+   // Set LED pin
+   pinMode(led_pin, OUTPUT);
+   pinMode(output_pin[0], OUTPUT);
+   pinMode(output_pin[1], OUTPUT);
+   pinMode(output_pin[2], OUTPUT);
+   pinMode(output_pin[3], OUTPUT);
+   pinMode(vcc, OUTPUT);
+   digitalWrite(output_pin[0], LOW);
+   digitalWrite(output_pin[1], LOW);
+   digitalWrite(output_pin[2], LOW);
+   digitalWrite(output_pin[3], LOW);
+   digitalWrite(vcc, HIGH);
+}
+void loop()
+{
+   uint8_t buf[VW_MAX_MESSAGE_LEN];
+   uint8_t buflen = VW_MAX_MESSAGE_LEN;
+   
+   if (vw_get_message(buf, &buflen))
+   {
+   
+   digitalWrite(led_pin, HIGH);
+   Serial.print("Got: ");
+   // Print message
+   for (int i = 0; i < buflen; i++)
+   {
+     Serial.print(char(buf[i]));
+     Serial.print(' ');
+     
+   }
+   
+   for (int j = 0; j < 4; j++){
+       if( buf[j] == 0x31 ){
+         output[j] = !output[j];
+       }
+   }
+   for (int k = 0; k < 4; k++){
+     if(output[k] == 1 ){
+       digitalWrite(output_pin[k], HIGH);}
+       else {
+         digitalWrite(output_pin[k], LOW);}
+   
+   Serial.println();
+   digitalWrite(led_pin, LOW);
+   }
+}
+delay(1000);
+}
---
+++ transmitter_ro_aut.ino
@@ -0,0 +1,65 @@
+#include <VirtualWire.h>
+
+
+const int led_pin = 13;
+const int transmit_pin = 12;
+const int input1_pin = 7;
+const int input2_pin = 6;
+const int input3_pin = 5;
+const int input4_pin = 4;
+int input1_value;
+int input2_value;
+int input3_value;
+int input4_value;
+const int vcc = A0;
+void setup()
+{
+   // Init
+   vw_set_tx_pin(transmit_pin);
+   vw_setup(2000); 
+   Serial.begin(9600); 
+   Serial.println("setup");
+   pinMode(led_pin, OUTPUT);
+   pinMode(input1_pin,INPUT);
+   pinMode(input2_pin,INPUT);
+   pinMode(input3_pin,INPUT);
+   pinMode(input4_pin,INPUT);
+   pinMode(vcc,OUTPUT);
+   digitalWrite(vcc, HIGH);
+   
+}
+void loop()
+{
+   
+   input1_value = digitalRead(input1_pin);
+   input2_value = digitalRead(input2_pin);
+   input3_value = digitalRead(input3_pin);
+   input4_value = digitalRead(input4_pin);
+   
+ char msg[4] = {'0','0','0','0'};
+   if (input1_value == 0){
+      msg[0] = '1';
+   }
+   if (input2_value == 0){
+      msg[1] = '1';
+   }
+   if (input3_value == 0){
+      msg[2] = '1';
+   }
+   if (input4_value == 0){
+      msg[3] = '1';
+   }
+   for (int i = 0; i < 4; i++)
+   {
+     Serial.print(char(msg[i]));
+     Serial.print(' ');
+     
+   }
+   Serial.println(' ');
+   Serial.println(digitalRead(input1_pin));
+   digitalWrite(led_pin, HIGH); 
+   vw_send((uint8_t *)msg, 4);
+   vw_wait_tx(); // Wait until the whole message is gone
+   digitalWrite(led_pin, LOW);
+   delay(1000);
+}
